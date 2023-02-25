5af97f9c972f569581dd2d3d6897743d65099fb0
Damjan Cvetko <damjan.cvetko@gmail.com>
2023-02-25 08:43:15 +0100

    Improve forwarder protocol handling

--- lib/proxy/client.go
+++ lib/proxy/client.go
@@ -3,15 +3,16 @@ package proxy
 import (
 	"crypto/tls"
 	"fmt"
-	"github.com/derickr/dbgp-tools/lib/connections"
-	"github.com/derickr/dbgp-tools/lib/logger"
-	"github.com/derickr/dbgp-tools/lib/protocol"
 	"io"
 	"net"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
+
+	"github.com/derickr/dbgp-tools/lib/connections"
+	"github.com/derickr/dbgp-tools/lib/logger"
+	"github.com/derickr/dbgp-tools/lib/protocol"
 )
 
 const sleepTimeout = time.Millisecond * 50
@@ -47,17 +48,6 @@ func (handler *ServerHandler) setupForwarder(conn net.Conn, initialPacket []byte
 		return err
 	}
 
-	defer func(closer io.Closer) {
-		err := closer.Close()
-		if err != nil {
-			handler.logger.LogUserError("proxy-client", clientConnection.GetKey(), "Closer didn't work: %v", err)
-		}
-	}(client)
-
-	if err != nil {
-		return err
-	}
-
 	handler.logger.LogUserInfo("proxy-client", clientConnection.GetKey(), "IDE connected")
 	reassembledPacket := fmt.Sprintf("%d\000%s", len(initialPacket)-1, initialPacket)
 	_, err = client.Write([]byte(reassembledPacket))
@@ -66,47 +56,58 @@ func (handler *ServerHandler) setupForwarder(conn net.Conn, initialPacket []byte
 	}
 
 	handler.logger.LogUserInfo("proxy-client", clientConnection.GetKey(), "Init forwarded, start pipe")
-	clientChan := make(chan error)
 	serverChan := make(chan error)
 
-	conn.SetDeadline(time.Now().Add(time.Second * 2))
+	defer func(closer io.Closer) {
+		err := closer.Close()
+		if err != nil {
+			handler.logger.LogUserError("proxy-client", clientConnection.GetKey(), "Closer didn't work: %v", err)
+		}
+		<-serverChan
+	}(client)
 
 	go func() {
-	restartCopy:
-		_, err = io.Copy(client, conn)
-		if opErr, ok := err.(*net.OpError); ok && opErr.Timeout() {
-			conn.SetDeadline(time.Now().Add(time.Second * 2))
-			goto restartCopy
+		// client read loop
+		for {
+			if _, err = io.Copy(conn, client); err != nil {
+				serverChan <- err
+				close(serverChan)
+				break
+			}
 		}
-		clientChan <- err
 	}()
 
-	go func() {
-	restartCopy:
-		_, err = io.Copy(conn, client)
-		if opErr, ok := err.(*net.OpError); ok && opErr.Timeout() {
-			conn.SetDeadline(time.Now().Add(time.Second * 2))
-			goto restartCopy
-		}
-		serverChan <- err
-	}()
-
+	// server read loop
+	reader := protocol.NewDbgpClient(conn, handler.logger)
 	for {
-		select {
-		case err = <-serverChan:
-			handler.logger.LogUserInfo("proxy-client", clientConnection.GetKey(), "IDE closed connection")
+		response, err, timeout := reader.ReadResponseWithTimeout(2 * time.Second)
 
-			/* If the IDE connection closes, then we should also close the Xdebug connection otherwise it
-			 * will hang https://github.com/derickr/dbgp-tools/issues/4 */
-			conn.Close()
-
-			return nil
-		case err = <-clientChan:
-			handler.logger.LogUserInfo("proxy-client", clientConnection.GetKey(), "Xdebug connection closed")
-			return nil
-		default:
-			time.Sleep(sleepTimeout)
+		if timeout {
+			// has client disconnected or had a fatal error?
+			if err, ok := <-serverChan; ok {
+				handler.logger.LogUserInfo("proxy-client", clientConnection.GetKey(), "IDE closed connection")
+				return fmt.Errorf("Client read error: %s", err)
+			}
+			continue
 		}
+
+		if err != nil {
+			// protocol error or EOF
+			// force close and return for cleanup
+			handler.logger.LogError("proxy-client", "Protocol error reading from server: %s", err)
+			conn.Close()
+			return nil
+		}
+
+		if packet := reader.FormatXML(response); packet != nil && packet.ShouldCloseConnection() {
+			// dbgp done, calling function will close connection or read next init packet from cloud
+			return nil
+		}
+
+		// forward packet
+		reassembledPacket := fmt.Sprintf("%d\000%s", len(response)-1, response)
+		client.Write([]byte(reassembledPacket))
+		// any error will re-appear on the top of the loop
 	}
 }
 
@@ -118,6 +119,8 @@ func (handler *ServerHandler) sendDetach(conn net.Conn) {
 	}
 }
 
+// The conn received is either a fresh Xdebug connection from a local Listen socket
+// or an initialized Cloud connection (cloudinit)
 func (handler *ServerHandler) Handle(conn net.Conn) error {
 	var key string
 	var connType string
@@ -162,8 +165,10 @@ ConnectionsLoop:
 
 		if ok {
 			handler.logger.LogUserInfo("proxy-client", key, "Found connection for %s '%s': %s", connType, key, client.FullAddress())
-			err = handler.setupForwarder(conn, []byte(response), client)
-			if err != nil {
+			if err := handler.setupForwarder(conn, []byte(response), client); err != nil {
+				// Error indicates
+				// - IDE connection failed or init packet send error - Xdebug/Cloud should be "released"
+				// - Client disconnected and left Xdebug/Cloud in non-stopped status
 				handler.logger.LogUserWarning("proxy-client", key, "Removed connection information for '%s': %s", key, err)
 				handler.connectionList.RemoveByKey(key)
 				handler.sendDetach(conn)
